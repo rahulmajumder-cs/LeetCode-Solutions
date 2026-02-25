cd2524f0d27f524b1f39a0e650e7354ad781b381
Rahul Majumder <rahulmajumder.cs@gmail.com>
2026-02-25 10:00:58 +0530

    Roman to Integer.cpp

---
+++ Arrays/Roman to Integer.cpp
@@ -0,0 +1,38 @@
+/*
+Problem Name: Roman to Integer
+Platform: LeetCode
+Language: C
+*/
+
+Code:
+#include <string.h>
+
+int value(char r) {
+    switch(r) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char *s) {
+    int total = 0;
+    int len = strlen(s);
+
+    for(int i = 0; i < len; i++) {
+        int current = value(s[i]);
+        int next = value(s[i + 1]);
+
+        if(current < next)
+            total -= current;
+        else
+            total += current;
+    }
+
+    return total;
+}
