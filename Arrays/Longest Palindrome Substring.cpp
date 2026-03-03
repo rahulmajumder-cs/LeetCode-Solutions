d5e985f7e48f76cbf3e5356d4873d3d838f600b0
Rahul Majumder <rahulmajumder.cs@gmail.com>
2026-03-03 23:34:58 +0530

    Create Longest Palindrome Substring.cpp

---
+++ Arrays/Longest Palindrome Substring.cpp
@@ -0,0 +1,42 @@
+/*
+Problem: Longest Palindrome Substring
+Language: C
+*/
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+char* longestPalindrome(char* s) {
+    int len = strlen(s);
+    if (len < 2) return s;
+
+    int start = 0;
+    int maxLen = 1;
+
+    for (int i = 0; i < len; i++) {
+        int left = i, right = i;
+        while (left >= 0 && right < len && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+        left = i;
+        right = i + 1;
+        while (left >= 0 && right < len && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    s[start + maxLen] = '\0';
+    return s + start;
+}
