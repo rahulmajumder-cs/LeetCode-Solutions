826a6f0837108d33b6fde78822a0e7530989dac1
Rahul Majumder <rahulmajumder.cs@gmail.com>
2026-03-07 09:55:21 +0530

    Update Add Two Number.cpp

--- Arrays/Add Two Number.cpp
+++ Arrays/Add Two Number.cpp
@@ -11,7 +11,6 @@ Language: c
  * };
  */
 
-
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode *dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
     struct ListNode *current = dummy;
