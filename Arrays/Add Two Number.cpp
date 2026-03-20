1b9295516d5417232bbb9e784846c76a4b8e55ca
Rahul Majumder <rahulmajumder.cs@gmail.com>
2026-03-20 22:43:24 +0530

    Initialize addTwoNumbers function with dummy node

--- Arrays/Add Two Number.cpp
+++ Arrays/Add Two Number.cpp
@@ -13,8 +13,6 @@ Language: c
 
 
 
-
-
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode *dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
     struct ListNode *current = dummy;
